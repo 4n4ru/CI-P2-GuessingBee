06046165f451d77d3da7c5013190c1cc13045008
Ana <ana.runje@live.com>
2021-06-16 23:03:39 +0000

    Style: style abc buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ function generateAbcBtns () {
     let alphabet = 'abcdefghijklmnopqrstuvwxyz';
     let html = '';
     for (const letter of alphabet) {
-        html += `<button class="btn btn-primary m-2">${letter}</button>`;
+        html += `<button class="btn btn-primary m-1 col-1 p-1">${letter}</button>`;
     }
     document.getElementById('abc-btns').innerHTML = html;
 }
