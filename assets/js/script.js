2fec2b10cb88bca877be882e8e16e0e753a9264a
Ana <ana.runje@live.com>
2021-06-17 23:55:47 +0000

    Feat: Add generateGameScreen function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,18 @@
 (function () {
 
+    let easyImages = [
+        'assets/images/flower-easy-1.png',
+        'assets/images/flower-easy-2.png',
+        'assets/images/flower-easy-3.png',
+        'assets/images/flower-easy-4.png',
+        'assets/images/flower-easy-5.png',
+        'assets/images/flower-easy-6.png',
+        'assets/images/flower-easy-7.png',
+        'assets/images/flower-easy-8.png'
+    ];
+    let score = 0;
+    let hScore = 0;
+
     document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('start-screen').style.display = 'none';
         document.getElementById('game-screen').style.display = 'none';
@@ -21,6 +34,9 @@
         document.getElementById('abc-btns').innerHTML = html;
     }
 
+    /**
+     * Show startScreen and add event listeners to buttons
+     */
     function startScreen() {
         document.getElementById('start-screen').style.display = 'flex';
         let buttons = document.getElementsByClassName('start-btn');
@@ -38,7 +54,25 @@
     }
 
     function generateGameScreen(gameType) {
-        
+        let difficulty = document.getElementById('difficulty');
+        difficulty.innerHTML = gameType;
+        difficulty.style.textTransform = 'uppercase';
+        document.getElementById('score').innerHTML = score;
+        document.getElementById('h-score').innerHTML = hScore;
+        let theme = getTheme ();
+        let phrase = getPhrase (theme);
+        document.getElementById('theme').innerHTML = theme;
+        let guessingPhrase = generateUnderscores(phrase);
+        document.getElementById('phrase').innerHTML = guessingPhrase;
+        if (gameType === 'easy') {
+            document.getElementById('flower-image').src = easyImages[7];
+        } else if (gameType === 'normal') {
+            document.getElementById('flower-image').src = normalImages[5];
+        } else {
+            document.getElementById('flower-image').src = hardImages[3];
+        }
+        generateAbcBtns();
+        showGame();
     }
 
     function getTheme() {
